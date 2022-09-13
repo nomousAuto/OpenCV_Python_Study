41d6f72ca37e53264fde58616541b77a552704c0
jihoonHuh <68678041+nomousAuto@users.noreply.github.com>
2022-09-13 10:27:30 +0900

    Update event_mouse.py

--- 04.Interface/event_mouse.py
+++ 04.Interface/event_mouse.py
@@ -2,30 +2,34 @@
 import numpy as np
 import cv2
 
-# 콜백 함수 - 이벤트 내용 출력
+# 콜백함수 - 이벤트 내용 출력
 def onMouse(event, x, y, flags, param):
-	if event == cv2.EVENT_LBUTTONDOWN:
-		print("마우스 왼쪽 버튼 누르기")
-	elif event == cv2.EVENT_RBUTTONDOWN:
-		print("마우스 오른쪽 버튼 누르기")
-	elif event == cv2.EVENT_RBUTTONUP:
-		print("마우스 오른쪽 버튼 떼기")
-	elif event == cv2.EVENT_LBUTTONDBLCLK:
-		print("마우스 왼쪽 버튼 더블클릭")
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("마우스 왼쪽 버튼 누르기")
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        print("마우스 오른쪽 버튼 누르기")
+    elif event == cv2.EVENT_RBUTTONUP:
+        print("마우스 오른쪽 버튼 떼기")
+    elif event == cv2.EVENT_LBUTTONDBLCLK:
+        print("마우스 왼쪽 버튼 더블클릭")
 
 # 초기 영상 생성
 image = np.full((200, 300), 255, np.uint8)
 
 # 윈도우 이름
 title1, title2 = "Mouse Event1", "Mouse Event2"
-# 영상보기
-cv2.imshow(title1, image)
-cv2.imshow(title2, image)
 
-# 마우스 콜백 함수
-cv2.setMouseCallback(title1, onMouse)
-# 키 이벤트 대기
-cv2.waitKey(0)
+while True:
+    # 영상 보기
+    cv2.imshow(title1, image)
+    cv2.imshow(title2, image)
+
+    # 마우스 콜백함수
+    cv2.setMouseCallback(title1, onMouse)
+
+    if cv2.waitKey() == 27:
+        print('ESC')
+        break
+
 # 열린 모든 윈도우 제거
 cv2.destroyAllWindows()
-
